47df99f0df16bd7fb49559f46d12879cdb89f50b
InjeJeong1 <wjddlswpas@gmail.com>
2024-06-24 18:42:31 +0900

    bigdata1.py

--- bigdata1.py
+++ bigdata1.py
@@ -1,58 +1,133 @@
+!pip install sklearn
+Looking in indexes: https://pypi.org/simple, https://us-python.pkg.dev/colab-wheels/public/simple/
+Collecting sklearn
+  Downloading sklearn-0.0.post1.tar.gz (3.6 kB)
+  Preparing metadata (setup.py) ... done
+Building wheels for collected packages: sklearn
+  Building wheel for sklearn (setup.py) ... done
+  Created wheel for sklearn: filename=sklearn-0.0.post1-py3-none-any.whl size=2344 sha256=e24eb5c956a7392f04815bf5c56a58bed831c50349cf4ae9d09bafe39fab3095
+  Stored in directory: /root/.cache/pip/wheels/14/25/f7/1cc0956978ae479e75140219088deb7a36f60459df242b1a72
+Successfully built sklearn
+Installing collected packages: sklearn
+Successfully installed sklearn-0.0.post1
+
+import numpy as np
 import pandas as pd
-import matplotlib
+
+from sklearn.datasets import load_boston
+boston = load_boston()
+/usr/local/lib/python3.8/dist-packages/sklearn/utils/deprecation.py:87: FutureWarning: Function load_boston is deprecated; `load_boston` is deprecated in 1.0 and will be removed in 1.2.
+
+    The Boston housing prices dataset has an ethical problem. You can refer to
+    the documentation of this function for further details.
+
+    The scikit-learn maintainers therefore strongly discourage the use of this
+    dataset unless the purpose of the code is to study and educate about
+    ethical issues in data science and machine learning.
+
+    In this special case, you can fetch the dataset from the original
+    source::
+
+        import pandas as pd
+        import numpy as np
+
+
+        data_url = "http://lib.stat.cmu.edu/datasets/boston"
+        raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None)
+        data = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
+        target = raw_df.values[1::2, 2]
+
+    Alternative datasets include the California housing dataset (i.e.
+    :func:`~sklearn.datasets.fetch_california_housing`) and the Ames housing
+    dataset. You can load the datasets as follows::
+
+        from sklearn.datasets import fetch_california_housing
+        housing = fetch_california_housing()
+
+    for the California housing dataset and::
+
+        from sklearn.datasets import fetch_openml
+        housing = fetch_openml(name="house_prices", as_frame=True)
+
+    for the Ames housing dataset.
+    
+  warnings.warn(msg, category=FutureWarning)
+  
+  print(boston.DESCR)
+  .. _boston_dataset:
+
+Boston house prices dataset
+---------------------------
+
+**Data Set Characteristics:**  
+
+    :Number of Instances: 506 
+
+    :Number of Attributes: 13 numeric/categorical predictive. Median Value (attribute 14) is usually the target.
+
+    :Attribute Information (in order):
+        - CRIM     per capita crime rate by town
+        - ZN       proportion of residential land zoned for lots over 25,000 sq.ft.
+        - INDUS    proportion of non-retail business acres per town
+        - CHAS     Charles River dummy variable (= 1 if tract bounds river; 0 otherwise)
+        - NOX      nitric oxides concentration (parts per 10 million)
+        - RM       average number of rooms per dwelling
+        - AGE      proportion of owner-occupied units built prior to 1940
+        - DIS      weighted distances to five Boston employment centres
+        - RAD      index of accessibility to radial highways
+        - TAX      full-value property-tax rate per $10,000
+        - PTRATIO  pupil-teacher ratio by town
+        - B        1000(Bk - 0.63)^2 where Bk is the proportion of black people by town
+        - LSTAT    % lower status of the population
+        - MEDV     Median value of owner-occupied homes in $1000's
+
+boston_df = pd.DataFrame(boston.data, columns = boston.feature_names)
+boston_df.head()
+         boston_df['PRICE'] = boston.target
+boston_df.head()
+         print('보스톤 주택 가격 데이터셋 크기:', boston_df.shape)
+보스톤 주택 가격 데이터셋 크기: (506, 14)
+
+boston_df.info()
+          from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
+#X, Y 분할하기
+Y = boston_df['PRICE']
+X = boston_df.drop(['PRICE'], axis = 1, inplace = False)
+
+#훈련용 데이터와 평가용 데이터 분할하기
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.3, random_state = 156)
+
+#선형 회귀 분석 : 모델 생성
+lr = LinearRegression()
+
+#선형 회귀 분석 : 모델 훈련
+lr.fit(X_train, Y_train)
+LinearRegression()
+
+#선형 회귀 분석 : 평가 데이터에 대한 예측 수행 -> 예측 결과 Y_predict 구하기
+Y_predict = lr.predict(X_test)
+
+mse = mean_squared_error(Y_test, Y_predict)
+rmse = np.sqrt(mse)
+
+print('MSE : {0:.3f}, RMSE : {1:.3f}'.format(mse, rmse))
+print('R^2(Variance score) : {0:.3f}'.format(r2_score(Y_test, Y_predict)))
+MSE : 17.297, RMSE : 4.159
+R^2(Variance score) : 0.757
+
+print('Y 절편 값:', lr.intercept_)
+print('회귀 계수 값:', np.round(lr.coef_, 1))
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-column_headers = ['x1','y1']
+fig, axs = plt.subplots(figsize = (16, 16), ncols = 3, nrows = 5)
 
-column_headers5482 = ['x5482','y5482']
+x_features = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT']
 
-column_headers1 = ['x2','y2']
-column_headers2 = ['x3','y3']
-column_headers3 = ['x4','y4']
-column_headers4 = ['x5','y5']
-
-
-csv = pd.read_csv('C:/Users/INJE/OneDrive - 순천대학교/바탕 화면/ebeam/lastdepo.csv', names = column_headers)
-
-csv5482 = pd.read_csv('C:/Users/INJE/OneDrive - 순천대학교/바탕 화면/ebeam/new1.csv', names = column_headers5482)
-
-csv1 = pd.read_csv('C:/Users/INJE/OneDrive - 순천대학교/바탕 화면/ebeam/dat.csv', names = column_headers1)
-csv2 = pd.read_csv('C:/Users/INJE/OneDrive - 순천대학교/바탕 화면/ebeam/dat2.csv', names = column_headers2)
-csv3 = pd.read_csv('C:/Users/INJE/OneDrive - 순천대학교/바탕 화면/ebeam/dat3.csv', names = column_headers3)
-csv4 = pd.read_csv('C:/Users/INJE/OneDrive - 순천대학교/바탕 화면/ebeam/dat4.csv', names = column_headers4)
-
-
-
-
-
-
-x = csv.loc[:,'x1']
-y = csv.loc[:,'y1']
-
-x5482 = csv5482.loc[:,'x5482']
-y5482 = csv5482.loc[:,'y5482']
-
-
-
-xa = csv1.loc[:,'x2']  #작년 결과
-ya = csv1.loc[:,'y2']
-xb = csv2.loc[:,'x3']
-yb = csv2.loc[:,'y3']
-xc = csv3.loc[:,'x4']
-yc = csv3.loc[:,'y4']
-xd = csv4.loc[:,'x5']
-yd = csv4.loc[:,'y5']
-
-
-plt.plot(x,y)
-
-plt.plot(x5482,y5482)
-
-plt.legend(('ver.2024','1.5482'))
-plt.title('energy,depth graph') 
-plt.xlabel('depth(um))')
-plt.ylabel('deposited Energy(eV/Angs./electron)')
-plt.xlim(0,1.2)
-plt.ylim(0,3.2)
-plt.grid(axis = 'y')
-plt.show()
+for i, feature in enumerate(x_features):
+    row = int(i/3)
+    col = i%3
+    sns.regplot(x = feature, y = 'PRICE', data = boston_df, ax = axs[row][col])
