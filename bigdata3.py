7706608ec1fce9d22e51ba8a7350c88dc8102893
InjeJeong1 <wjddlswpas@gmail.com>
2024-06-24 18:46:04 +0900

    bigdata3.py

--- bigdata3.py
+++ bigdata3.py
@@ -1,58 +1,129 @@
+import numpy as np
 import pandas as pd
-import matplotlib
-import matplotlib.pyplot as plt
 
-column_headers = ['x1','y1']
+from sklearn.datasets import load_breast_cancer
 
-column_headers5482 = ['x5482','y5482']
+b_cancer = load_breast_cancer()
 
-column_headers1 = ['x2','y2']
-column_headers2 = ['x3','y3']
-column_headers3 = ['x4','y4']
-column_headers4 = ['x5','y5']
+print(b_cancer.DESCR)
+.. _breast_cancer_dataset:
 
+Breast cancer wisconsin (diagnostic) dataset
+--------------------------------------------
 
-csv = pd.read_csv('C:/Users/INJE/OneDrive - 순천대학교/바탕 화면/ebeam/lastdepo.csv', names = column_headers)
+**Data Set Characteristics:**
 
-csv5482 = pd.read_csv('C:/Users/INJE/OneDrive - 순천대학교/바탕 화면/ebeam/new1.csv', names = column_headers5482)
+    :Number of Instances: 569
 
-csv1 = pd.read_csv('C:/Users/INJE/OneDrive - 순천대학교/바탕 화면/ebeam/dat.csv', names = column_headers1)
-csv2 = pd.read_csv('C:/Users/INJE/OneDrive - 순천대학교/바탕 화면/ebeam/dat2.csv', names = column_headers2)
-csv3 = pd.read_csv('C:/Users/INJE/OneDrive - 순천대학교/바탕 화면/ebeam/dat3.csv', names = column_headers3)
-csv4 = pd.read_csv('C:/Users/INJE/OneDrive - 순천대학교/바탕 화면/ebeam/dat4.csv', names = column_headers4)
+    :Number of Attributes: 30 numeric, predictive attributes and the class
 
+    :Attribute Information:
+        - radius (mean of distances from center to points on the perimeter)
+        - texture (standard deviation of gray-scale values)
+        - perimeter
+        - area
+        - smoothness (local variation in radius lengths)
+        - compactness (perimeter^2 / area - 1.0)
+        - concavity (severity of concave portions of the contour)
+        - concave points (number of concave portions of the contour)
+        - symmetry
+        - fractal dimension ("coastline approximation" - 1)
 
+        The mean, standard error, and "worst" or largest (mean of the three
+        worst/largest values) of these features were computed for each image,
+        resulting in 30 features.  For instance, field 0 is Mean Radius, field
+        10 is Radius SE, field 20 is Worst Radius.
 
+        - class:
+                - WDBC-Malignant
+                - WDBC-Benignb_cancer_df = pd.DataFrame(b_cancer.data, columns = b_cancer.feature_names)
 
+b_cancer_df['diagnosis'] = b_cancer.target
 
+b_cancer_df.head()
+print('유방암 진단 데이터셋 크기: ', b_cancer_df.shape)
+유방암 진단 데이터셋 크기:  (569, 31)
 
-x = csv.loc[:,'x1']
-y = csv.loc[:,'y1']
+b_cancer_df.info()
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
 
-x5482 = csv5482.loc[:,'x5482']
-y5482 = csv5482.loc[:,'y5482']
+b_cancer_scaled = scaler.fit_transform(b_cancer.data)
 
+print(b_cancer.data[0])
+ print(b_cancer_scaled[0])
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
+#X, Y 설정하기
+Y = b_cancer_df['diagnosis']
+X = b_cancer_scaled
 
-xa = csv1.loc[:,'x2']  #작년 결과
-ya = csv1.loc[:,'y2']
-xb = csv2.loc[:,'x3']
-yb = csv2.loc[:,'y3']
-xc = csv3.loc[:,'x4']
-yc = csv3.loc[:,'y4']
-xd = csv4.loc[:,'x5']
-yd = csv4.loc[:,'y5']
+#훈련용 데이터와 평가용 데이터 분할하기
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.3, random_state = 0)
 
+#로지스틱 회귀 분석: (1) 모델 생성
+lr_b_cancer = LogisticRegression()
 
-plt.plot(x,y)
+#로지스틱 회귀 분석: (2) 모델 훈련
+lr_b_cancer.fit(X_train, Y_train)
+LogisticRegression()
 
-plt.plot(x5482,y5482)
+#로지스틱 회귀 분석: (3) 평가 데이터에 대한 예측 수행 -> 예측 결과 Y_predict 구하기
+Y_predict = lr_b_cancer.predict(X_test)
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score
 
-plt.legend(('ver.2024','1.5482'))
-plt.title('energy,depth graph') 
-plt.xlabel('depth(um))')
-plt.ylabel('deposited Energy(eV/Angs./electron)')
-plt.xlim(0,1.2)
-plt.ylim(0,3.2)
-plt.grid(axis = 'y')
-plt.show()
+#오차 행렬
+confusion_matrix(Y_test, Y_predict)
+array([[ 60,   3],
+       [  1, 107]])
+       
+accuracy = accuracy_score(Y_test, Y_predict)
+precision = precision_score(Y_test, Y_predict)
+recall = recall_score(Y_test, Y_predict)
+f1 = f1_score(Y_test, Y_predict)
+roc_auc = roc_auc_score(Y_test, Y_predict)
+
+print('정확도: {0:.3f}, 정밀도: {1:.3f}, 재현율: {2:.3f}, F1: {3:.3f}'.format(accuracy,precision,recall,f1))
+정확도: 0.977, 정밀도: 0.973, 재현율: 0.991, F1: 0.982
+
+print('ROC_AUC: {0:.3f}'.format(roc_auc))
+ROC_AUC: 0.972
+import numpy as np
+import pandas as pd
+
+pd.__version__
+1.3.5
+
+#피처 이름 파일 읽어오기
+feature_name_df = pd.read_csv('/features.txt', sep = '\s+', header = None, names = ['index', 'feature_name'], engine = 'python')
+
+feature_name_df.head()
+feature_name_df.shape
+(561, 2)
+
+#index 제거하고, feature_name만 리스트로 저장
+feature_name = feature_name_df.iloc[:, 1].values.tolist()
+
+feature_name[:5]
+['tBodyAcc-mean()-X',
+ 'tBodyAcc-mean()-Y',
+ 'tBodyAcc-mean()-Z',
+ 'tBodyAcc-std()-X',
+ 'tBodyAcc-std()-Y']
+ 
+ X_train = pd.read_csv('/X_train.txt', delim_whitespace=True, header=None, encoding='latin-1')
+X_train.columns = feature_name
+X_test = pd.read_csv('/X_test.txt', delim_whitespace=True, header=None, encoding='latin-1')
+X_test.columns = feature_name
+Y_train = pd.read_csv('/Y_train.txt', sep='\s+', header = None, names = ['action'], engine = 'python')
+Y_test = pd.read_csv('/Y_test.txt', sep='\s+', header = None, names = ['action'], engine = 'python')
+
+X_train.shape, Y_train.shape, X_test.shape, Y_test.shape
+((7352, 561), (7352, 1), (2947, 561), (2947, 1))
+
+X_train.head()
+print(Y_train['action'].value_counts())
+label_name_df = pd.read_csv('/activity_labels.txt', sep = '\s+', header = None, names = ['index', 'label'], engine = 'python')
+label_name = label_name_df.iloc[:, 1].values.tolist()
